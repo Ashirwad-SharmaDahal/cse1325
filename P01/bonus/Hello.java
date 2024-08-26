6a238f35762b36938866882ce6a6f9ca5630959c
Ashirwad Sharma Dahal <axs9463@mavs.uta.edu>
2024-08-25 22:26:49 -0500

    assignment

--- P01/bonus/Hello.java
+++ P01/bonus/Hello.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class BonusHello{
+public class Hello{
 	public static void main(String[] args){
 	    Scanner in = new Scanner(System.in);
 	    System.out.print("What is your name?");
