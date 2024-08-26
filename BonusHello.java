63f95d2026590d4da867c49f42394c18b56f7857
Ashirwad Sharma Dahal <axs9463@mavs.uta.edu>
2024-08-25 21:42:49 -0500

    assignment

---
+++ BonusHello.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class BonusHello{
+	public static void main(String[] args){
+	    Scanner in = new Scanner(System.in);
+	    System.out.print("What is your name?");
+	    String a;
+	    a=in.nextLine();
+	    System.out.println("Hello "+(a)+"!");
+
+	}
+}
