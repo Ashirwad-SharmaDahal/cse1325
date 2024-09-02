f3b851fb938daa9b950d341127b659af341a7c47
Ashirwad Sharma Dahal <axs9463@mavs.uta.edu>
2024-09-02 16:05:24 -0500

    assignment

---
+++ P02/full_credit/Coin.java
@@ -0,0 +1,15 @@
+public class Coin{
+	private Denomination denomination;
+	private int year;
+	public Coin(Denomination denomination, int year){
+	    this.denomination = denomination;
+	    this.year = year;
+	}
+	public Denomination getDenomination(){
+	return denomination;
+	}
+	public int getYear(){
+	return year;
+	}
+
+}
---
+++ P02/full_credit/Purse.java
@@ -0,0 +1,27 @@
+public class Purse{
+	public static void main(String[] args){
+	Coin[] coins = new Coin[]{
+	new Coin(Denomination.PENNY, 2020),
+	new Coin(Denomination.NICKLE, 1889),
+	new Coin(Denomination.DIME, 1996),
+	new Coin(Denomination.QUARTER, 2004),
+	};
+	int totalValue = 0;
+	int earliestYear = Integer.MAX_VALUE;
+	int latestYear = Integer.MIN_VALUE;
+
+	for(Coin coin : coins){
+	totalValue += coin.getDenomination().getValue();
+	int year = coin.getYear();
+	if(year<earliestYear){
+	earliestYear = year;
+	}
+	if(year>latestYear){
+	latestYear = year;
+	}
+    }
+     System.out.println("Total Value: " + totalValue + " cents");
+        System.out.println("Earliest Year: " + earliestYear);
+        System.out.println("Latest Year: " + latestYear);
+	}
+}
