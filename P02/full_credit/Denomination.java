40ec15a4bf7a0492ade9df805a48796de5d1923e
Ashirwad Sharma Dahal <axs9463@mavs.uta.edu>
2024-09-02 17:31:15 -0500

    assignment

---
+++ P02/full_credit/Denomination.java
@@ -0,0 +1,13 @@
+ public enum Denomination {
+    PENNY(1), NICKEL(5), DIME(10), QUARTER(25);
+
+    private int value;
+
+    Denomination(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
