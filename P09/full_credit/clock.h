d640267f6380a72f1fdb08ec1e400ba7aec9f286
Ashirwad Sharma Dahal <axs9463@mavs.uta.edu>
2024-11-04 21:35:06 -0600

    assignment

---
+++ P09/full_credit/clock.h
@@ -0,0 +1,23 @@
+#ifndef CLOCK_H
+#define CLOCK_H
+
+#include <iostream>
+#include <stdexcept>
+#include <iomanip>
+
+class Clock {
+public:
+    
+    Clock(int hours, int minutes, int seconds);
+    virtual ~Clock() = default; 
+
+    void print() const;   
+    void tic();          
+
+private:
+    int _hours;
+    int _minutes;
+    int _seconds;
+};
+
+#endif
