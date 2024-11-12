6955b910b610190d92c40273a14c40b43ae4c665
Ashirwad Sharma Dahal <axs9463@mavs.uta.edu>
2024-11-12 00:16:52 -0600

    assignment

---
+++ P10/full_credit/Purse.cpp
@@ -0,0 +1,46 @@
+#include "Purse.h"
+
+Purse::Purse(int pounds, int shillings, int pence)
+    : _pounds(pounds), _shillings(shillings), _pence(pence) {
+    rationalize();
+}
+
+void Purse::rationalize() {
+    if (_pence >= 12) {
+        _shillings += _pence / 12;
+        _pence %= 12;
+    }
+    if (_shillings >= 20) {
+        _pounds += _shillings / 20;
+        _shillings %= 20;
+    }
+}
+
+Purse Purse::operator+(const Purse& other) const {
+    return Purse(_pounds + other._pounds, _shillings + other._shillings, _pence + other._pence);
+}
+
+Purse Purse::operator-(const Purse& other) const {
+    return Purse(_pounds - other._pounds, _shillings - other._shillings, _pence - other._pence);
+}
+
+Purse& Purse::operator+=(const Purse& other) {
+    _pounds += other._pounds;
+    _shillings += other._shillings;
+    _pence += other._pence;
+    rationalize();
+    return *this;
+}
+
+Purse& Purse::operator-=(const Purse& other) {
+    _pounds -= other._pounds;
+    _shillings -= other._shillings;
+    _pence -= other._pence;
+    rationalize();
+    return *this;
+}
+
+std::ostream& operator<<(std::ostream& os, const Purse& purse) {
+    os << "£" << purse._pounds << " " << purse._shillings << "s " << purse._pence << "d";
+    return os;
+}
