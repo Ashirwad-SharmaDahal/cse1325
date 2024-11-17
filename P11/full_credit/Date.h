e5f9ec1cfba4f42bda832a28edd2e1042eb0ed90
Ashirwad Sharma Dahal <axs9463@mavs.uta.edu>
2024-11-17 16:41:24 -0600

    assignment

---
+++ P11/full_credit/Date.h
@@ -0,0 +1,43 @@
+#ifndef DATE_H
+#define DATE_H
+
+#include <iostream>
+#include<iomanip>
+
+class Date{
+private:
+	int _year, int _month, int _day;
+
+	int compare(const Date& other) const{
+		if(_year != other._year){
+			return _year < other._year? -1 : 1;
+		}
+		if(_month != other._month){
+			return _month < other._month? -1 : 1;	
+		}
+		if(_day != other._day){
+			return _day < other._day? -1 : 1;
+		}	
+		return 0;
+	}
+
+public:
+	Date(int year = 1970, int month = 1, int day = 1)
+		: _year(year), _month(month), _day(day) {}
+
+	bool operator==(const Date& other) const { return compare(other) == 0; }
+	bool operator!=(const Date& other) const {return compare(other) !=  0; }
+	bool operator<(const Date& other) const { return compare(other) < 0; }
+	bool operator<=(const Date& other) const{ return compare(other) <= 0; }
+	bool operator>(const Date& other) const { return compare(other) > 0; }
+	bool operator>=(const Date& other) const { return compare(other) >= 0; }		
+		
+	friend std::ostream& operator<<(std::ostream& os, const Date& date){
+		char old_fill = os.fill();
+		os << std::setw(4) << std::setfill('0') << date._year << "/"
+		   << std::setw(2) << std::setfill('0') << date._month << "/"
+		   << std::setw(2) << std::setfill('0') << date._day << "/"
+		os.fill(old_fill);
+		return os;
+	}
+}	
